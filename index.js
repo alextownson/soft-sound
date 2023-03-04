3ce51eb15c2512ffd320cf9fe90353ecaa7cc513
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-03-04 11:32:22 -0500

    Updated loader.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import seqSketch from "./modules/sequencer.js";
 import keySketch from "./modules/keyboard.js";
 
-setTimeout(function(){ 
-    document.getElementById('loader').style.display = 'none';
-}, 5000);
+window.addEventListener('load', () => {
+    document.querySelector('#loader').style.height = window.innerHeight + 'px';
+    console.log('hello')
+    setTimeout(function(){ 
+        document.querySelector('#loader').style.opacity = 0;
+        document.querySelector('#synth').style.opacity = 1;
+    }, 5000);
+})
+
 
 // Instance mode:
 // https://github.com/processing/p5.js/wiki/Global-and-instance-mode
